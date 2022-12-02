5768ba9e2c8e6b2416bb2ad29f8175317bfa2caf
Eugen Svirbutovich <4workacc@gmail.com>
2022-12-02 15:45:49 +0300

    feat: cli_args__ready

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    let args = process.argv;
+    if ( args.length > 2 ) {
+        for ( let i=2; i<args.length; i++) {
+            if ( args[i][0] === '-') {
+                console.log( `${args[i].substring(2, args[i].length)} is ${args[i+1]}`);
+                i += 1;
+            }            
+        }
+    }
 };
 
 parseArgs();
