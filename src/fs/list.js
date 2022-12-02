b49cbbbf6a436ec9a07663eab769bab67288586a
Eugen Svirbutovich <4workacc@gmail.com>
2022-12-02 12:48:41 +0300

    feat: fs_list_ready

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
 const list = async () => {
-    // Write your code here 
+    let folder_exist = fs.existsSync('files');
+    
+    if ( !folder_exist ) {
+        throw new Error('FS operation failed');
+    }
+
+    let files_list = fs.readdirSync('files', ()=>{});
+
+    console.log( files_list )
 };
 
 await list();
