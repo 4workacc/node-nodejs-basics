e2cb102f5a7a3a4cf8876bb6cfd786196172bf7d
Eugen Svirbutovich <4workacc@gmail.com>
2022-12-02 12:41:03 +0300

    feat: fs_delete_ready

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+    let file_exist = fs.existsSync('./files/fileToRemove.txt');
+
+    if ( !file_exist ) {
+        throw new Error('FS operation failed')
+    }
+
+    fs.unlink('./files/fileToRemove.txt', ()=>{})
 };
 
 await remove();
