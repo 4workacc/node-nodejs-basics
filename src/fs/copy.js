aeeca74885de87ce0bbd5613f60c7020323a6f69
Eugen Svirbutovich <4workacc@gmail.com>
2022-12-02 12:33:41 +0300

    Update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    let files_exist = fs.existsSync('files');
+    let copy_exits = fs.existsSync('files_copy');
+
+    if ( !files_exist || copy_exits ) {
+        throw new Error('FS operation failed')
+    }
+   
+    fs.cp('./files/hello.txt', './files_copy/hello.txt', ()=>{})
 };
 
 copy();
