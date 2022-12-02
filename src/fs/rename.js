3a1cf51812554638bd32b1d117a981c0da0668fb
Eugen Svirbutovich <4workacc@gmail.com>
2022-12-02 12:37:56 +0300

    feat: fs_rename_ready

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    let file_exist = fs.existsSync('./files/wrongFilename.txt');
+    let newFile_exits = fs.existsSync('./filesproperFilename.md');
+
+    if ( !file_exist || newFile_exits ) {
+        throw new Error('FS operation failed')
+    }
+
+    fs.rename('./files/wrongFilename.txt','./files/properFilename.md', ()=>{} )
 };
 
 await rename();
