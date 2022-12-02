8f1001599ccb0b2b5debad890f0fa2dd5f34e0f8
Eugen Svirbutovich <4workacc@gmail.com>
2022-12-02 12:51:30 +0300

    feat:fs_read__ready

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    let file_exist = fs.existsSync('./files/fileToRead.txt');
+
+    if ( !file_exist ) {
+        throw new Error('FS operation failed')
+    }
+
+    console.log( fs.readFileSync('./files/fileToRead.txt').toString())
 };
 
 await read();
