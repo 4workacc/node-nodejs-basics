3434879a15663ae358952a131e02ad116b6f3692
Eugen <4workacc@gmail.com>
2022-12-03 12:23:11 +0300

    feat:zlib__decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,11 @@
+import zlib from 'zlib';
+import fs from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+    var gzip = zlib.createUnzip();
+    var r = fs.createReadStream('src/zip/files/archive.gz');
+    var w = fs.createWriteStream('src/zip/files/fileToCompress.txt');
+    r.pipe(gzip).pipe(w); 
 };
 
 await decompress();
