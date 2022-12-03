c8c4d9fae9c6886e131deadfe291d887b5d15c44
Eugen <4workacc@gmail.com>
2022-12-03 11:56:54 +0300

    feat: hash__ready

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    let textData = fs.readFileSync('src/hash/files/fileToCalculateHashFor.txt').toString();
+    let hash = crypto.createHash('sha256');
+    let data = hash.update(textData, 'utf-8');
+    let gen_hash = data.digest('hex');
+    console.log( gen_hash )
 };
 
 await calculateHash();
+
+
