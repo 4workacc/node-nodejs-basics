5a5290c1f63be91573117d5aced97c147acc8e68
4workacc <58693140+4workacc@users.noreply.github.com>
2022-12-04 12:08:10 +0300

    feat: stream_read__ready

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    let rStream = new fs.ReadStream('./src/streams/files/fileToRead.txt', { encoding: 'utf-8'});
+
+    rStream.on('readable', ()=>{
+        let dat = rStream.read();
+        if ( dat != null ) {
+            process.stdout.write(dat);
+        }
+    })
 };
 
 await read();
