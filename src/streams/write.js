78ba80d680bf80cdaedd3f936f4cc2bc34b77489
4workacc <58693140+4workacc@users.noreply.github.com>
2022-12-04 12:18:14 +0300

    feat: streams_write__ready

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
-const write = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+
+const write = async () => {   
+    let wStream = new fs.WriteStream('./src/streams/files/fileToWrite.txt', {encodinf: 'utf-8'});
+    process.stdin.on('data', data=>{
+        wStream.write(data.toString(), ()=>{});
+    })
 };
 
 await write();
