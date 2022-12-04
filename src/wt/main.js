303490ed688d7fd1b991bd15f36e19273d5d9365
4workacc <58693140+4workacc@users.noreply.github.com>
2022-12-04 19:25:35 +0300

    feat: worker_ready

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import os from 'os';
+import { parentPort, Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+   let cpus = os.cpus().length;
+   let answers = [];
+   let readyAnswers = 0;
+   for ( let i =0; i<cpus; i++) {
+      const wor = new Worker('./src/wt/worker.js', { workerData : { num: 10 + i} });
+      wor.on('message', msg=>{
+        let num = msg.worker.split('_')[1]/1-10;        
+        answers[num] = {
+            status: 'resolved',
+            data: msg.data
+        };
+        readyAnswers += 1;
+        if ( readyAnswers === 7) {
+            console.log(answers)
+        }
+      })
+    }        
 };
 
 await performCalculations();
