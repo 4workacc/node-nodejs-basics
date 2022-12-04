aa33f4afb8b3654188e387fb377d9c702c817ba8
4workacc <58693140+4workacc@users.noreply.github.com>
2022-12-04 13:01:40 +0300

    feat: cp_ready

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,11 @@
+import { exec, fork, spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const ch = fork('./src/cp/files/script.js');
+    ch.on('message', (ms)=>{
+        process.stdout.write(args)   
+    })
+    ch.send(process.stdin)
 };
 
 spawnChildProcess();
