931fb529117e47ef0f1de01832785494686619ae
4workacc <58693140+4workacc@users.noreply.github.com>
2022-12-04 12:40:16 +0300

    feat: streams_transform__ready

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
-const transform = async () => {
-    // Write your code here 
-};
+import {Transform} from 'stream';
+
+const transform = async () => {    
+    process.stdin.pipe(reverse).pipe(process.stdout)};
+
+const reverse = new Transform({
+    transform(chunk, encoding, callback){
+        this.push(chunk.toString().split('').reverse().join(''));
+        callback();
+    }
+})
+
 
 await transform();
